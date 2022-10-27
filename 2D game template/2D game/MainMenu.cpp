cbabb55d9362f28dd5b030530eb35020878b3550
Turmo11 <guillem.turmo.00@gmail.com>
2022-10-27 10:39:44 +0200

    1st screen colliders done

--- 2D game template/2D game/MainMenu.cpp
+++ 2D game template/2D game/MainMenu.cpp
@@ -48,9 +48,9 @@ bool MainMenu::Start()
 
 	box = game->ui->AddInputBox(buttonfont, Color::black, fpoint(20.0f, 20.0f));
 	
-	player = game->entities->AddPlayer("Ron", { 400,-500 }, 0);
+	player = game->entities->AddPlayer("Ron", { 100, 700 }, 0);
 
-	camera = game->entities->AddCamara(fpoint(400, -500));
+	camera = game->entities->AddCamara(fpoint(100, -500));
 	camera->Follow(player, 0.1f, 10.0f, true);
 	
 	return true;
