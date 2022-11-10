70fe6b4204c6c150d3c65aa595c8f1bf74952547
Turmo11 <45201984+Turmo11@users.noreply.github.com>
2022-11-10 15:01:18 +0100

    Started on 2nd part

---
+++ 2D game template/Game/Assets/tiled/forest_map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="forest_map" tilewidth="3112" tileheight="1280" tilecount="5" columns="5">
+ <image source="../images/forest_3rd_iteration_complete.png" width="15560" height="1280"/>
+</tileset>
